57502521a7b9fe03bb14332e6c234f3f095f59a8
RaiYashPramod <raiyashpramod16@gmail.com>
2024-10-26 01:28:08 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <>
+      <div className='h-screen bg-gradient-to-b from-black to-green-900 flex items-center justify-center'>
+        <h3 className='text-3xl font-bold text-center text-white'>Spotify Clone Incoming!!</h3>
+      </div>
+    </>
+  )
+}
+
+export default App
